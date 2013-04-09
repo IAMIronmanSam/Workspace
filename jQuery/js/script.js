184fcdf2c9df2dd10e11e6497a8912008aab04cf
Arivazhagan Ambigapathi <arivu86@gmail.com>
2013-04-09 11:07:09 +0530

    Qunit Integration

--- jQuery/js/script.js
+++ jQuery/js/script.js
@@ -1,8 +1,21 @@
-$(function (){
+   //$(document).ready(function() {
+    function add(c){
+    var f = c + 1;
+    return f;
+	}
 
-});
+	function subs (b) {
+		var a = b-1;
+		return a;
+	}
 
-function cssselect (elmt) {
-	
-		
-}
+	function multi (d) {
+		var g = d*2;
+		return g;
+	}
+
+	function divd (e) {
+		var h = e/2
+		return h ;
+	}
+ //   });//
--- jQuery/js/test.js
+++ jQuery/js/test.js
@@ -1,4 +1,49 @@
-$(function (){
+module ("Hello jQuery")
+test( "hello test", function() {
+  ok( 1 == "1", "Passed!" );
+  console.log("Hello Qunit")
+})
 
-	console.log("Hi");
-});
+module ("Addition")
+test( "Addition", function() {
+   var actual1 = add(2);
+    equal(actual1, 3, "Value is correct");
+    console.log("Addition is True");
+
+     var actual2 = add(5);
+    equal(actual2, 6, "Value is not correct");
+    console.log("Addition is False")
+})
+
+module ("Subtraction")
+test( "Subtraction", function() {
+   var actual1 = subs(2);
+    equal(actual1, 1, "Value is correct");
+    console.log("Subtraction is True");
+
+     var actual2 = subs(5);
+    equal(actual2, 6, "Value is not correct");
+    console.log("Subtraction is False")
+})
+
+module ("Multiplication")
+test( "Multiplication", function() {
+   var actual1 = multi(2);
+    equal(actual1, 4, "Value is correct");
+    console.log("Multiplication is True");
+
+     var actual2 = add(5);
+    equal(actual2, 7, "Value is not correct");
+    console.log("Multiplication is False")
+})
+
+module ("Division")
+test( "Division", function() {
+   var actual1 = divd(2);
+    equal(actual1, 1, "Value is correct");
+    console.log("Division is True");
+
+     var actual2 = divd(5);
+    deepEqual(actual2, 2.5, "Value is not correct");
+    console.log("Division is False")
+})
