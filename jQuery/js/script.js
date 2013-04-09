b66a78be3d33927fa8aaaed51f11b50a003cba7c
Arivazhagan Ambigapathi <arivu86@gmail.com>
2013-04-09 15:23:08 +0530

    DOM Testing

--- jQuery/js/script.js
+++ jQuery/js/script.js
@@ -1,21 +1,52 @@
    //$(document).ready(function() {
-    function add(c){
-    var f = c + 1;
-    return f;
+   	var testnamspc = {
+    add:function(c){
+    	if(isNaN(c)){
+		console.log(c + " is not a number");
+ 	}else{
+	 	var f = c + 1;
+	    return f;
+		console.log(c + " is a number");
+ }
+    
+	},
+	subs: function(b) {
+			if(isNaN(b)){
+		console.log(b + " is not a number");
+ 	}else{
+		if (b>1)
+		{var a = b-1;
+		return a;}
+		else{
+			console.log("Value > 1");
+		}
 	}
+	},
 
-	function subs (b) {
-		var a = b-1;
-		return a;
-	}
-
-	function multi (d) {
+	multi:function(d) {
+			if(isNaN(d)){
+		console.log(d + " is not a number ");
+ 	}else{
 		var g = d*2;
 		return g;
 	}
+	},
 
-	function divd (e) {
+	divd:function(e) {
+			if(isNaN(e)){
+		console.log(e + " is not a number");
+ 	}else{
 		var h = e/2
 		return h ;
 	}
+},
+	dom:function (target) {
+		var z = $(target).text("hello");
+		var t =  $(target).text();
+		return t;
+}
+
+};
+
+testnamspc.subs('j');
  //   });//
--- jQuery/js/test.js
+++ jQuery/js/test.js
@@ -6,44 +6,46 @@ test( "hello test", function() {
 
 module ("Addition")
 test( "Addition", function() {
-   var actual1 = add(2);
+   var actual1 = testnamspc.add(2);
     equal(actual1, 3, "Value is correct");
     console.log("Addition is True");
-
-     var actual2 = add(5);
+     var actual2 = testnamspc.add(5);
     equal(actual2, 6, "Value is not correct");
-    console.log("Addition is False")
+    console.log("Addition is False");
 })
 
 module ("Subtraction")
 test( "Subtraction", function() {
-   var actual1 = subs(2);
+   var actual1 = testnamspc.subs(2);
     equal(actual1, 1, "Value is correct");
     console.log("Subtraction is True");
-
-     var actual2 = subs(5);
+     var actual2 = testnamspc.subs(5);
     equal(actual2, 6, "Value is not correct");
-    console.log("Subtraction is False")
+    console.log("Subtraction is False");
 })
 
 module ("Multiplication")
 test( "Multiplication", function() {
-   var actual1 = multi(2);
+   var actual1 = testnamspc.multi(2);
     equal(actual1, 4, "Value is correct");
     console.log("Multiplication is True");
-
-     var actual2 = add(5);
+     var actual2 = testnamspc.multi(5);
     equal(actual2, 7, "Value is not correct");
-    console.log("Multiplication is False")
+    console.log("Multiplication is False");
 })
 
 module ("Division")
 test( "Division", function() {
-   var actual1 = divd(2);
+   var actual1 = testnamspc.divd(2);
     equal(actual1, 1, "Value is correct");
     console.log("Division is True");
-
-     var actual2 = divd(5);
+     var actual2 = testnamspc.divd(5);
     deepEqual(actual2, 2.5, "Value is not correct");
-    console.log("Division is False")
+    console.log("Division is False");
+})
+module ("DOM")
+test("do some dom stuff",function() {
+    var actual1 = testnamspc.dom('p')  
+    deepEqual(actual1,"hello");
+    console.log("Dom Passed");
 })
