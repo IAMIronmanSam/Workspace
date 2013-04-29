8228269a30038bd4c362320bb3576085d8cc3340
Arivazhagan <arivu86@gmail.com>
2013-04-29 19:53:47 +0530

    Get Co-Ordinates

--- jQuery/js/pattern.js
+++ jQuery/js/pattern.js
@@ -1,30 +1,35 @@
-var Home = {};
+var Board = {};
 
-Home = {
-	Welcome:function (_member) {
-		var mem = (_member == "sakthi") ? true : false;
-		return _member;
-		console.log("Welcome "+_member+"!");
-	},
+Board = {
 	Dots:function  (_type) {
 		if(_type <= "9")
 		{
-		for (var i = 1; i <=_type; i++) {
-			$('#Board').append("<div class=\"dots\" id="+i+">"+i+"</div>");
+			for (var i = 1; i <=_type; i++) {
+				$('#Board').append("<div class=\"dots\" id="+i+">"+i+"</div>");
+			}
 		}
-	}
 	else{
-		for (var i = 1; i <=9; i++) {
-			$('#Container').append("<div class=\"dots\" id="+i+"></div>");
+			for (var i = 1; i <=9; i++) {
+				$('#Container').append("<div class=\"dots\" id="+i+"></div>");
+			}
+			alert("Maximun 9 is only allowed");
 		}
-		alert("Maximun 9 is only allowed");
-	}
 	},
-
-
+	getPosition:function  () {
+			var vue = [];
+			$("#ip input:text").each(function(){
+				vue.push($(this).val());
+				return vue;
+			});
+			console.log("Co-Ordinates: "+vue);	
+	},
+	DrawLines:function  (_coord) {
+		
+		
+	},
 };
 
 
 
-Home.Welcome("sakthi");
+
 
